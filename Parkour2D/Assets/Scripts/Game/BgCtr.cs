e4c00e4c41479e4a7f1d5027a59a8282a0bb004e
TastSong <TastSong@163.com>
2020-10-17 16:34:43 +0800

    add new res

--- Parkour2D/Assets/Scripts/Game/BgCtr.cs
+++ Parkour2D/Assets/Scripts/Game/BgCtr.cs
@@ -8,20 +8,24 @@ public class BgCtr : MonoBehaviour
     public bool isStopBgMove = false;
     public float speed = 200f;
 
-    private float bgPosX;
+    private float bgStartPosX;
+    private float bgStartPosY;
+    private float bgStartPosZ;
 
     private void Start() {
         if (bgGo.Length > 1) {
-            bgPosX = bgGo[1].transform.position.x;
+            bgStartPosX = bgGo[1].transform.position.x;
+            bgStartPosY = bgGo[1].transform.position.y;
+            bgStartPosZ = bgGo[1].transform.position.z;
         }
     }
 
     private void Update() {
         if (!isStopBgMove) {
             for (int i = 0; i < bgGo.Length; i++) {
-                bgGo[i].transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
-                if (bgGo[i].transform.position.x < -bgPosX) {
-                    bgGo[i].transform.position = new Vector3(bgPosX, 0, 0);
+                bgGo[i].transform.position = new Vector3(bgGo[i].transform.position.x - speed * Time.deltaTime, bgStartPosY, bgStartPosZ);
+                if (bgGo[i].transform.position.x < -bgStartPosX) {
+                    bgGo[i].transform.position = new Vector3(bgStartPosX, bgStartPosY, bgStartPosZ);
                 }
             }
         }      
--- Parkour2D/Assets/Scripts/Game/GroundCtr.cs
+++ Parkour2D/Assets/Scripts/Game/GroundCtr.cs
@@ -9,19 +9,23 @@ public class GroundCtr : MonoBehaviour
     public float speed = 400f;
 
     private float itemPosX = 1920f;
+    private float itemPosY = 1920f;
+    private float itemPosZ = 1920f;
 
     private void Start() {
         if (GroundItems.Length > 1) {
             itemPosX = GroundItems[1].transform.position.x;
+            itemPosY = GroundItems[1].transform.position.y;
+            itemPosZ = GroundItems[1].transform.position.z;
         }
     }
 
     private void Update() {
         if (!isStopGroundMove) {
             for (int i = 0; i < GroundItems.Length; i++) {
-                GroundItems[i].gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+                GroundItems[i].gameObject.transform.position = new Vector3(GroundItems[i].gameObject.transform.position.x - speed * Time.deltaTime, itemPosY, itemPosZ);
                 if (GroundItems[i].gameObject.transform.position.x < -itemPosX) {
-                    GroundItems[i].gameObject.transform.position = new Vector3(itemPosX, 0, 0);
+                    GroundItems[i].gameObject.transform.position = new Vector3(itemPosX, itemPosY, itemPosZ);
                     GroundItems[i].ChangePlatform();
                 }
             }
