2ff91279f2eb3abadb5c6ca9a54e79409c016795
TastSong <TastSong@163.com>
2020-10-18 19:10:53 +0800

    add DoTween and GameOverAnim

---
+++ Parkour2D/Assets/Scripts/Anim/GameOverAnim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GameOverAnim : MonoBehaviour
+{
+    public Image gameOverBg;
+    private void OnEnable() {
+        gameOverBg.fillAmount = 0;
+        DOTween.To(() => gameOverBg.fillAmount, x => gameOverBg.fillAmount = x, 1f, 0.3f).SetEase(Ease.InOutCubic);
+    }
+}
