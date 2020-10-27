39608c39669bfcf837c87d5082ac767e5ed189b5
TastSong <TastSong@163.com>
2020-10-27 23:00:56 +0800

    add res

---
+++ Parkour2D/Assets/Scripts/Anim/SequenceFrameAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SequenceFrameAnim : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Image selfImage;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selfImage = GetComponent<Image>();
+    }
+
+    private void Update() {
+        selfImage.sprite = spriteRenderer.sprite;
+    }
+}
