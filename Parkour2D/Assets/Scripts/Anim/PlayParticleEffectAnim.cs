82636dc6c9f4169cbf6605c5fcc8e6c0155a4fc4
TastSong <TastSong@163.com>
2020-11-04 21:31:43 +0800

    add Particle effect

---
+++ Parkour2D/Assets/Scripts/Anim/PlayParticleEffectAnim.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticleEffectAnim : MonoBehaviour
+{
+    private void OnEnable() {
+        GetComponent<ParticleSystem>().Play();
+    }
+}
